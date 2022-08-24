dd1ed56247fe64b72b5e1e61d1a7b14689a14941
udhai-20 <udhayaroger511@gmail.com>
2022-08-24 23:25:07 +0530

    login and signup

---
+++ script/signup.js
@@ -0,0 +1,23 @@
+let collectdata = () => {
+  let UserObj = {
+    Email: document.getElementById("email").value,
+    Zip: document.getElementById("Zip").value,
+  };
+
+  if (UserObj.Email == "" || UserObj.Zip == "") {
+    alert("Please Enter All Input Fields");
+  } else if (UserObj.Zip.length < 5 || UserObj.Zip.length > 5) {
+    let data = document.getElementById("error");
+    data.innerText = "Pleas Enter Valid Zip code";
+    data.style.color = "red";
+    data.style.marginTop = "5px";
+  } else {
+    document.getElementById("email").value = "";
+    document.getElementById("Zip").value = "";
+    window.location.href = "login.html";
+    alert("SignUp Sucessfuly");
+    localStorage.setItem("UserDetail", JSON.stringify(UserObj));
+  }
+  console.log("  UserObj:", UserObj);
+};
+function displayData() {}
