bd978c197e9750d8922c6dc6c45ec9e604a4d236
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2022-08-27 21:30:26 +0530

    updated login and signup in homepage

--- Home Page/scripts/swiper.js
+++ Home Page/scripts/swiper.js
@@ -13,3 +13,22 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  let login_showData = JSON.parse(localStorage.getItem("Login_details"));
+  console.log('login_showData:', login_showData);
+
+  let login = document.querySelector(".login");
+  let signup = document.querySelector(".signup>button");
+  if (login_showData!=null) {
+    login.innerHTML = login_showData.name;
+    signup.innerText = 'Log Out'; 
+    signup.setAttribute('class','logout');
+  }
+  let logout = document.querySelector('.logout');
+  logout.addEventListener("click",(event) => {
+    // event.preventDefault();
+    localStorage.setItem('Login_details',JSON.stringify(null));
+    window.location.href = 'signup.html';
+  });
+
